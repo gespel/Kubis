f66704317e417e261a9da5622c0ec835c0f4769e
gespelor <heimbrodt@uni-potsdam.de>
2024-05-21 20:20:33 +0000

    feierabend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build().expect("Error while building client!");
+    let res = client
+        .get("https://127.0.0.1:8001/")
+        .header("Accept", "application/json")
+        .send()
+        .await?;
+
+    println!("{}", res.text().await.expect("asd"));
+
+    Ok(())
 }
